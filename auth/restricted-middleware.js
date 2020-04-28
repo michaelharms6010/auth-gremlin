59ad179339ca1a3ba61e88415af62dee99088d10
Michael Harms <michael.harms6010@gmail.com>
2020-04-28 12:04:15 -0500

    removes restricted middleware

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,21 +1,3 @@
-const jwt = require('jsonwebtoken');
-const secrets = require('../config/secrets');
-
 module.exports = (req, res, next) => {
-  const token = req.headers.authorization;
-  const secret = secrets.jwtSecret;
-
-  if (token) {
-    jwt.verify(token, secret, (err, decodedToken) => {
-      if(err) {
-        res.status(401).json('Unauthorized')
-      } else {
-        req.decodedJwt = decodedToken;
-        console.log(decodedToken)
-        next();
-      }
-    });
-  } else {
-    res.status(401).json({ you: 'failed' });
-  }
+    res.status(401).json({ heh: 'nice try dude' });
 };
